a49a55e6915e09b337c772d9344c7646f92d2314
Oleg Rybin <you@example.com>
2024-08-05 16:52:58 -0400

    Adding a toggle to unly display sutvivors in the bar chart

--- src/BarChart.js
+++ src/BarChart.js
@@ -1,27 +1,59 @@
 import * as d3 from "d3";
 
 export class BarChart {
+  #isSurvivorsOnly = false;
+  #ageGroups = [
+    { groupTitle: "0-14", min: 0, max: 14 },
+    { groupTitle: "15-24", min: 15, max: 24 },
+    { groupTitle: "25-64", min: 25, max: 64 },
+    { groupTitle: "65 and over", min: 65, max: 1000 },
+  ];
+  #width = 600;
+  #height = 400;
+  #marginTop = 40;
+  #marginRight = 20;
+  #marginBottom = 30;
+  #marginLeft = 40;
+  #svg;
+  #x;
+  #y;
+  #ageGroupBuckets;
+  #ageGroupBucketsSurvivors;
+
   constructor(elementId, data) {
     this.#createBarChart(elementId, data);
   }
 
   #createBarChart(elementId, data) {
     const element = document.getElementById(elementId);
+    const barChartToggle = document.getElementById("barChartToggle");
+    barChartToggle.setAttribute("aria-pressed", this.#isSurvivorsOnly);
 
-    const ageGroups = [
-      { groupTitle: "0-14", min: 0, max: 14 },
-      { groupTitle: "15-24", min: 15, max: 24 },
-      { groupTitle: "25-64", min: 25, max: 64 },
-      { groupTitle: "65 and over", min: 65, max: 1000 },
-    ];
+    barChartToggle.addEventListener("click", (e) => {
+      const button = e.target.classList.contains("toggle-button")
+        ? e.target
+        : e.target.closest(".toggle-button");
 
-    const ageGroupTitles = ageGroups.map((ageGroup) => ageGroup.groupTitle);
+      this.#isSurvivorsOnly = !this.#isSurvivorsOnly;
+
+      button.setAttribute("aria-pressed", this.#isSurvivorsOnly);
+
+      if (this.#isSurvivorsOnly) {
+        this.#updateChart(this.#ageGroupBucketsSurvivors);
+      } else {
+        this.#updateChart(this.#ageGroupBuckets);
+      }
+    });
+
+    const ageGroupTitles = this.#ageGroups.map(
+      (ageGroup) => ageGroup.groupTitle
+    );
 
     const passengersWithAge = data.filter(
       (passenger) => passenger.Age !== null
     );
 
-    const ageGroupBuckets = ageGroups.map(({ groupTitle, min, max }) => {
+    this.#ageGroupBuckets = this.#ageGroups.map(({ groupTitle, min, max }) => {
       return {
         groupTitle,
         passengers: passengersWithAge.filter(
@@ -30,79 +62,103 @@ export class BarChart {
       };
     });
 
-    const ageAxisScale = ageGroupBuckets.reduce((maxLength, ageGroupBucket) => {
-      return Math.max(maxLength, ageGroupBucket.passengers.length);
-    }, 0);
+    this.#ageGroupBucketsSurvivors = this.#ageGroupBuckets.map(
+      (ageGroupBucket) => {
+        return {
+          groupTitle: ageGroupBucket.groupTitle,
+          passengers: ageGroupBucket.passengers.filter(
+            (passenger) => passenger.Survived === 1
+          ),
+        };
+      }
+    );
+
+    const ageAxisScale = this.#ageGroupBuckets.reduce(
+      (maxLength, ageGroupBucket) => {
+        return Math.max(maxLength, ageGroupBucket.passengers.length);
+      },
+      0
+    );
 
     const ageAxisScaleRounded = Math.ceil(ageAxisScale / 100) * 100;
 
-    const width = 600;
-    const height = 400;
-    const marginTop = 80;
-    const marginRight = 20;
-    const marginBottom = 30;
-    const marginLeft = 40;
-
-    const x = d3
+    this.#x = d3
       .scaleBand()
       .domain(ageGroupTitles)
-      .range([marginLeft, width - marginRight])
+      .range([this.#marginLeft, this.#width - this.#marginRight])
       .padding(0.3);
 
-    const y = d3
+    this.#y = d3
       .scaleLinear()
       .domain([0, ageAxisScaleRounded])
-      .range([height - marginBottom, marginTop]);
+      .range([this.#height - this.#marginBottom, this.#marginTop]);
 
-    const svg = d3
+    this.#svg = d3
       .create("svg")
-      .attr("width", width)
-      .attr("height", height)
-      .attr("viewBox", [0, 0, width, height])
+      .attr("width", this.#width)
+      .attr("height", this.#height)
+      .attr("viewBox", [0, 0, this.#width, this.#height])
       .attr("style", "max-width: 100%; height: auto;");
 
-    svg
-      .append("text")
-      .attr("x", marginLeft)
-      .attr("y", 40)
-      .attr("font-size", "18px")
-      .text("Number of passengers in each age group");
-
-    svg
+    this.#svg
       .append("g")
       .selectAll()
-      .data(ageGroupBuckets)
+      .data(this.#ageGroupBuckets)
       .enter()
       .append("rect")
       .attr("class", "chart-bar")
-      .attr("x", (d) => x(d.groupTitle))
-      .attr("y", (d) => y(d.passengers.length))
-      .attr("width", x.bandwidth())
-      .attr("height", (d) => height - y(d.passengers.length) - marginBottom);
+      .attr("x", (d) => this.#x(d.groupTitle))
+      .attr("y", (d) => this.#y(d.passengers.length))
+      .attr("width", this.#x.bandwidth())
+      .attr(
+        "height",
+        (d) => this.#height - this.#y(d.passengers.length) - this.#marginBottom
+      );
 
-    svg
+    this.#svg
       .append("g")
       .selectAll()
-      .data(ageGroupBuckets)
+      .data(this.#ageGroupBuckets)
       .enter()
       .append("text")
       .attr("class", "chart-label")
-      .attr("x", (d) => x(d.groupTitle) + x.bandwidth() / 2)
-      .attr("y", (d) => y(d.passengers.length) - 5)
-      .attr("font-size", "12px")
-      .attr("text-anchor", "middle")
+      .attr("x", (d) => this.#x(d.groupTitle) + this.#x.bandwidth() / 2)
+      .attr("y", (d) => this.#y(d.passengers.length) - 5)
       .text((d) => d.passengers.length);
 
-    svg
+    this.#svg
       .append("g")
-      .attr("transform", `translate(0,${height - marginBottom})`)
-      .call(d3.axisBottom(x));
+      .attr("class", "chart-axis")
+      .attr("transform", `translate(0,${this.#height - this.#marginBottom})`)
+      .call(d3.axisBottom(this.#x));
 
-    svg
+    this.#svg
       .append("g")
-      .attr("transform", `translate(${marginLeft},0)`)
-      .call(d3.axisLeft(y));
+      .attr("class", "chart-axis")
+      .attr("transform", `translate(${this.#marginLeft},0)`)
+      .call(d3.axisLeft(this.#y));
 
-    element.append(svg.node());
+    element.append(this.#svg.node());
+  }
+
+  #updateChart(data) {
+    this.#svg
+      .selectAll(".chart-bar")
+      .data(data)
+      .transition()
+      .duration(500)
+      .attr("y", (d) => this.#y(d.passengers.length))
+      .attr(
+        "height",
+        (d) => this.#height - this.#y(d.passengers.length) - this.#marginBottom
+      );
+
+    this.#svg
+      .selectAll(".chart-label")
+      .data(data)
+      .transition()
+      .duration(500)
+      .attr("y", (d) => this.#y(d.passengers.length) - 5)
+      .text((d) => d.passengers.length);
   }
 }
